4daa63b50946e676ca0d44cee98f23ac69ec174a
JAMES HILLMANN <s-JHILLMANN@lwsd.org>
2020-12-02 10:16:46 -0800

    Done

--- 2020/Day 2/day2.py
+++ 2020/Day 2/day2.py
@@ -7,6 +7,29 @@ def checkPassword(min,max,letter,words):
         return True
     else:
         return False
+def checkPasswordPolicy(test_list):
+    correctPasswords = 0
+    for line in test_list:
+        x = line.split(" ")
+        num = x[0].split("-")
+        letter = x[1][0]
+        if checkPassword(int(num[0]), int(num[1]), letter, x[2]):
+            correctPasswords +=1
+    print(correctPasswords)
+
+def checkPositions(test_list):
+    correctPasswords = 0
+    for line in test_list:
+        x = line.split(" ")
+        num = x[0].split("-")
+        letter = x[1][0]
+        for i in range(0, len(num)):
+            num[i] = int(num[i])
+
+        if (x[2][num[0]-1] == letter) ^ (x[2][num[1]-1] == letter):
+            correctPasswords +=1
+
+    print(correctPasswords)
 
 
 if __name__ == "__main__":
@@ -15,14 +38,5 @@ if __name__ == "__main__":
     testContent = test.read()
     test_list = testContent.splitlines()
     test.close()
-    correctPasswords = 0
-    incorrectPasswords = 0
-    for line in test_list:
-        x = line.split(" ")
-        num = x[0].split("-")
-        letter = x[1]
-        if checkPassword(int(num[0]), int(num[1]), letter[0], x[2]):
-            correctPasswords +=1
-        else:
-            incorrectPasswords+=1
-    print(correctPasswords)
+    checkPasswordPolicy(test_list)
+    checkPositions(test_list)
